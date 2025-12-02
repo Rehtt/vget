3a986a3cb17d92a1e89c02c25f1eb05162375d70
Yumin Gui <guiyumin@protonmail.com>
2025-12-02 14:01:20 -0800

    feat: add version command to display version information

---
+++ internal/cli/version.go
@@ -0,0 +1,21 @@
+package cli
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/guiyumin/vget/internal/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("vget v%s %s/%s\n", version.Version, runtime.GOOS, runtime.GOARCH)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
